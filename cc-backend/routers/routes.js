08fe8c26b0330175cc2ccfdebe97bf418825d5b2
Ferdi761 <ferdiansyahachmad29@gmail.com>
2023-06-02 19:11:24 +0700

    add initial commit

---
+++ cc-backend/routers/routes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+const { helloWorld } = require('../controllers/helloWorld')
+
+
+router.route('/').get(helloWorld)
+
+module.exports = router
