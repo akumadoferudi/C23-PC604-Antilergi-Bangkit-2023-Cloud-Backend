496ce9c9b3715799d47b0b853bac91a99811032d
Achmad Ferdiansyah <74496640+akumadoferudi@users.noreply.github.com>
2023-06-13 13:53:21 +0700

    Revert "tes"

--- cc-backend/app.js
+++ cc-backend/app.js
@@ -1,30 +1,29 @@
 const express = require('express')
 const app = express()
-// const { Sequelize } = require('sequelize')
+const { Sequelize } = require('sequelize')
 require('dotenv').config()  // for read env file
-const router = require('./routers/routes')
-const HOST = process.env.HOST || 'localhost'
-const PORT = process.env.PORT || 8000
+const router = require('./router/routes')
+const port = process.env.PORT || 8000
 
 // db env
-// const db = process.env.DB || 'antilergi'
-// const username = process.env.USERNAME || 'admin'
-// const password = process.env.PASSWORD || 'admin123'
-// const dbHost = process.env.DBHOST || 'localhost'
-// const dbDriver = process.env.DBDRIVER || 'mysql'
+const db = process.env.DB || 'antilergi'
+const username = process.env.USERNAME || 'admin'
+const password = process.env.PASSWORD || 'admin123'
+const dbHost = process.env.DBHOST || 'localhost'
+const dbDriver = process.env.DBDRIVER || 'mysql'
 
 // connect to DB orm
-// const sequelize = new Sequelize('antilergi', 'user', 'password', {
-//     host: 'dbHost',
-//     dialect: 'dbDriver',
-//   })
+const sequelize = new Sequelize('antilergi', 'user', 'password', {
+    host: 'dbHost',
+    dialect: 'dbDriver',
+  })
 
-//   try {
-//     await sequelize.authenticate();
-//     console.log('Connection has been established successfully.');
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-//   }
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 
 app.use('/', router)
 
@@ -32,10 +31,4 @@ app.use('/', router)
 //   res.send('Hello World')
 // })
 
-// const syncModel = async () => {
-//   await sequelize.sync({ force: true });
-//   console.log("All models were synchronized successfully.");
-// }
-// syncModel();
-
-app.listen(PORT, () => { console.log(`listening on http://${HOST}:${PORT}`) })
+app.listen(port, () => { console.log(`listening on http://localhost:${port}`) })
--- cc-backend/db/collections/articlesCollections.js
+++
@@ -1,3 +0,0 @@
-const articlesCollections = [{
-    name:
-}]
