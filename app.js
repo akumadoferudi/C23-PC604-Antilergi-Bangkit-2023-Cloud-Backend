1c8903e02d06f79f18df1ff0354c53b73ea3678c
Noveliyo Frendika <novelyof900@gmail.com>
2023-06-16 20:41:09 +0700

    Cloud

--- app.js
+++ app.js
@@ -5,21 +5,11 @@ const router = require('./routers/routes')
 const articleRoutes = require('./routers/articleRoutes')
 const userRoutes = require('./routers/userRoutes')
 PORT = 8080
-HOST = 'localhost'
 
-// app.use('/', router)
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/', router)
 app.use('/articles/', articleRoutes)
 app.use('/user/', userRoutes)
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World')
-// })
-
-// const syncModel = async () => {
-//   await sequelize.sync({ force: true });
-//   console.log("All models were synchronized successfully.");
-// }
-// syncModel();
-
-app.listen(PORT, () => { console.log(`listening on http://${HOST}:${PORT}`) })
+app.listen(PORT, () => { console.log(`listening on PORT:${PORT}`) })
